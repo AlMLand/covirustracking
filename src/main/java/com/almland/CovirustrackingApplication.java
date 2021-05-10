7bb3529c76f89f3c5c0da0c7157a484c53219614
AlMLand <morlandalex@googlemail.com>
2021-05-10 20:39:07 +0200

    Starting form

---
+++ src/main/java/com/almland/CovirustrackingApplication.java
@@ -0,0 +1,13 @@
+package com.almland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovirustrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovirustrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/almland/CovirustrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovirustrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
