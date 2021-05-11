73f4df4aea1440f1500304309c93067721e93f3f
AlMLand <morlandalex@googlemail.com>
2021-05-11 15:21:53 +0200

    Added a scheduling function first hour of every day

--- src/main/java/com/almland/services/CoVirusDataService.java
+++ src/main/java/com/almland/services/CoVirusDataService.java
@@ -1,6 +1,7 @@
 package com.almland.services;
 
 import java.io.IOException;
+import java.io.Reader;
 import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -11,28 +12,29 @@ import javax.annotation.PostConstruct;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CoVirusDataService {
 
 	private final String CO_VIRUS_DATASOURCE_JOHNS_HOPKINS_UNIVERSITY_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv";
-	
+
+	@Scheduled(cron = "* * 1 * * *")
 	@PostConstruct
 	public void fetchCoVirusData() throws IOException, InterruptedException {
 		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create(CO_VIRUS_DATASOURCE_JOHNS_HOPKINS_UNIVERSITY_URL))
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(CO_VIRUS_DATASOURCE_JOHNS_HOPKINS_UNIVERSITY_URL))
 				.build();
 		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-		
-		Iterable<CSVRecord> records = CSVFormat.DEFAULT
-				.withFirstRecordAsHeader().parse(new StringReader(response.body()));
-		
-		for(CSVRecord csvRecord : records) {
+
+		Reader reader = new StringReader(response.body());
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader);
+
+		for (CSVRecord csvRecord : records) {
 			String provinceState = csvRecord.get("Province/State");
 			System.out.println(provinceState);
 		}
 	}
-	
+
 }
