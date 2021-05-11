7efc82f9f8218d41a533344464085ed5ca213ff2
AlMLand <morlandalex@googlemail.com>
2021-05-11 18:17:09 +0200

    Modified the method getHome()

--- src/main/java/com/almland/controller/CoVirusController.java
+++ src/main/java/com/almland/controller/CoVirusController.java
@@ -1,13 +1,23 @@
 package com.almland.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.almland.services.CoVirusDataService;
 
 @Controller
 public class CoVirusController {
 
+	@Autowired
+	private CoVirusDataService coVirusDataService;
+	
 	@GetMapping
-	public String getHome() {
+	public String getHome(Model model) {
+		model.addAttribute("TotalReportedDeltaTheDayBefore", coVirusDataService.getTotalReportedDeltaTheDayBefore());
+		model.addAttribute("totalReportedCases", coVirusDataService.getTotalReportedCases());
+		model.addAttribute("locationStatistics", coVirusDataService.getAllStats());
 		return "home";
 	}
 	
